a655bc0e6b46a0d284e7bb29833d664060eeca8b
Edgardo Reyes <edgardoreyesbelmonte@gmail.com>
2026-01-13 09:58:33 -0400

    microservicio producto y catalogo corregido

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
-import { AppModule } from '../app.module';
+import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
